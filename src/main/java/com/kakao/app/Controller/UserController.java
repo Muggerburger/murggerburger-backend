920d827e81572c677f38014e5d07f5f737f90e67
chaemin8600 <chaemin8600@gachon.ac.kr>
2024-02-17 00:31:37 +0900

    commit test

---
+++ src/main/java/com/kakao/app/Controller/UserController.java
@@ -0,0 +1,8 @@
+package com.kakao.app.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
--- src/main/java/com/kakao/app/Entity/UserEntity.java
+++ src/main/java/com/kakao/app/Entity/UserEntity.java
@@ -1,25 +1,22 @@
-package com.kakao.app.kakao;
+package com.kakao.app.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
+
 @Getter
-@Setter
+@Entity
 @NoArgsConstructor
-public class KakaoEntity {
+@AllArgsConstructor
+@Table(name="user")
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id", updatable = false)
-    private Long id;
-
-    @Column(name="nickname", nullable = false)
+    private Long userId; // user_id
     private String nickname;
-
-    @Column(name = "time", nullable = true)
     private Long time;
-
 }
